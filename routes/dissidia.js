8824f9bc76ea56dcde7449e0cfecfc787ed3f3a2
Bryn Norwood <Bryn.Norwood@nscc.ca>
2024-09-23 11:58:34 -0300

    initial commit

--- routes/dissidia.js
+++ routes/dissidia.js
@@ -2,6 +2,20 @@ import express from 'express';
 
 const router = express.Router();
 
+// Multer setup
+const storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+   cb(null, 'public/images/'); // save uploaded files in `public/images` folder
+   },
+   filename: function (req, file, cb) {
+   const ext = file.originalname.split('.').pop(); // get file extension
+   const uniqueFilename = Date.now() + '-' + Math.round(Math.random() * 1000) + '.' + ext; // generate unique filename - current timestamp + random number between 0 and 1000.
+   cb(null, uniqueFilename);
+   }
+  });
+  const upload = multer({ storage: storage });
+  
+
 
 //...api/dissidia
 router.get('/', (req, res) => {
@@ -12,13 +26,12 @@ router.get('/', (req, res) => {
 //...api/dissidia/all
 router.get('/all', (req, res) => {
      //to do... get all characters for database
-    res.send('get all Dissidia route');
+    res.send('get all dissidia route');
   });
 
-  //api/dissidia/create
+  //...api/dissidia/create
 router.post('/create', (req, res) => {
-
-     //to do... add new characters for database
+    //to do... add new characters for database
     res.send('add a new Dissidia route');
   });
 
