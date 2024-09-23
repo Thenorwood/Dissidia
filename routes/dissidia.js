15fcae6d1eef46c07835f2631f7c0172a4efaa77
Bryn Norwood <Bryn.Norwood@nscc.ca>
2024-09-23 20:07:06 -0300

    scaffolding

--- routes/dissidia.js
+++ routes/dissidia.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import multer from 'multer';
+import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
+ 
+
+
 
 // Multer setup
 const storage = multer.diskStorage({
@@ -18,21 +27,52 @@ const storage = multer.diskStorage({
 
 
 //...api/dissidia
-router.get('/', (req, res) => {
-  res.send('dissidia route');
-});
+//router.get('/', (req, res) => {
+ // res.send('dissidia route');
+//});
 
 
 //...api/dissidia/all
-router.get('/all', (req, res) => {
-     //to do... get all characters for database
-    res.send('get all dissidia route');
+router.get('/all', async (req, res) => {
+     const characters = await prisma.contact.findMany();
+
+     res.json(contacts);
   });
 
+
+  //get a character by id
+router.get('/get/:id', async (req, res) => {
+  const id  = req.params.id;
+
+  //validate id is a num
+  if(isNaN(id)){
+    return res.status(400).json({ message: 'Invalid  character id'});
+  }
+})
+
   //...api/dissidia/create
-router.post('/create', (req, res) => {
-    //to do... add new characters for database
-    res.send('add a new Dissidia route');
+router.post('/create', upload.single('image'), async(req, res) => {
+    const filename = req.file ? req.file.filename : null;
+    const { Name, Series, Deity, Description } = req.body;
+
+    if(!Name || !Series || !Deity ) {
+      // to-do:delete uploaded file
+      return res.status(400).json({message: 'Required fields must have a value.'});
+    }
+
+
+    const character = await prisma.character.create({
+      data: {
+        Name: Name,
+        Series: Series,
+        Deity: Deity,
+        Description: Description,
+        filename: filename
+      },
+    });
+    
+    
+    res.send('add character');
   });
 
   //testing github classroom
